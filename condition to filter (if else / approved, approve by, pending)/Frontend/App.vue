6bc274dff33198b8d462c083a3196b33f1425d56
JTI-MAC <jtimac2022@gmail.com>
2022-12-13 11:22:20 +0800

    kodigo

---
+++ condition to filter (if else / approved, approve by, pending)/Frontend/App.vue
@@ -0,0 +1,13 @@
+<v-flex md2 style="padding:">
+                <v-select
+                  v-model="filterStatusType"
+                  :items="filterStatusTypes"
+                  label="Status Type"
+                  :nudge-right="40"
+                  offset-y
+                  full-width
+                  min-width="290px"
+                  solo
+                  clearable
+                ></v-select>
+              </v-flex>
